677ee7776b0edf5775bbed414aca14ffde58dcfc
Chris Harris <c.harris@communicationinnovations.com.au>
2020-01-16 15:29:23 +1000

    moved the autoloader call in test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
+require_once(dirname(__DIR__ ). '/vendor/autoload.php');
 
 \SebastianBergmann\Comparator\Factory::getInstance()->register(new \MyCLabs\Enum\PHPUnit\Comparator());
-
-require_once(dirname(__DIR__ ). '/vendor/autoload.php');
