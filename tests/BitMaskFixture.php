daa0b0a21629acf9b8baa94443ad21feaa2498b5
c-harris <c.harris@communicationinnovations.com.au>
2020-03-20 14:34:31 +1000

    extend fixtures with a zero value and document fixtures

--- tests/BitMaskFixture.php
+++ tests/BitMaskFixture.php
@@ -7,6 +7,26 @@ namespace Cruxinator\BitMask\Tests;
 
 use Cruxinator\BitMask\BitMask;
 
+/**
+ * @method self     ONE()
+ * @method bool     isONE()
+ * @method self     setONE(bool $onOrOff)
+ * @method self     TWO()
+ * @method bool     isTWO()
+ * @method self     setTWO(bool $onOrOff)
+ * @method self     FOUR()
+ * @method bool     isFOUR()
+ * @method self     setFOUR(bool $onOrOff)
+ * @method self     EIGHT()
+ * @method bool     isEIGHT()
+ * @method self     setEIGHT(bool $onOrOff)
+ * @method self     SIXTEEN()
+ * @method bool     isSIXTEEN()
+ * @method self     setSIXTEEN(bool $onOrOff)
+ * @method self     THIRTYTWO()
+ * @method bool     isTHIRTYTWO()
+ * @method self     setTHIRTYTWO(bool $onOrOff)
+ */
 class BitMaskFixture extends BitMask
 {
     const ONE       = 1;
---
+++ tests/BitMaskZeroFixture.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Cruxinator\BitMask\Tests;
+
+/**
+ * @method self    ZERO()
+ * @method bool    isZERO()
+ * @method self    setZERO(bool $onOrOff)
+ */
+class BitMaskZeroFixture extends BitMaskFixture
+{
+    const ZERO = 0;
+}
