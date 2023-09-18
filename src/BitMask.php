61190aeae770df0b5ee79a919b2bb76710964879
Christopher Harris <c.harris@multitel.com.au>
2023-09-18 15:04:44 +1000

    moved php version for master to 8.1 and appended some basic code cleanup to go woith it.

--- src/BitMask.php
+++ src/BitMask.php
@@ -6,6 +6,7 @@ namespace Cruxinator\BitMask;
 
 use BadMethodCallException;
 use MyCLabs\Enum\Enum;
+use ReflectionException;
 use UnexpectedValueException;
 
 abstract class BitMask extends Enum
@@ -13,52 +14,17 @@ abstract class BitMask extends Enum
     // Because 0 is the identity element when ORing a bitmask, we have to special-case it when ANDing bitmasks
     // (akin to ordinary division by 0, the addition identity element on integers/rationals/reals)
 
-    protected function isFlag(int $flag): bool
+    protected static function assertValidValueReturningKey($value): string
     {
-        return 0 === $flag ? 0 === $this->value : (($this->value & $flag) == $flag);
-    }
-
-    protected function isComponentOfFlag(int $flag): bool
-    {
-        return 0 === $flag ? $flag === $this->value : (($this->value & $flag) == $this->value);
-    }
-
-    protected function setFlag(int $flag, bool $value)
-    {
-        if ($value) {
-            $this->value = 0 === $flag ? 0 : $this->value | $flag;
-        } else {
-            $this->value = 0 === $flag ? $this->value : $this->value & ~$flag;
+        if (!static::isValid($value)) {
+            throw new UnexpectedValueException("Value '$value' is not part of the enum " . static::class);
         }
 
-        return $this;
-    }
-
-    /**
-     * @param $name
-     * @param $arguments
-     *
-     * @throws \ReflectionException
-     *
-     * @return bool|self
-     */
-    public function __call($name, $arguments)
-    {
-        $array = static::toArray();
-        $regexBase = '/(isComponentOf|is|set)(%s)/m';
-        $regexFull = sprintf($regexBase, implode('$|', array_keys($array)));
-        preg_match($regexFull, $name, $match);
-        if (count($match) > 0 && $match[0] === $name) {
-            return $this->{$match[1].'Flag'}($array[$match[2]], $arguments[0] ?? true);
-        }
-
-        throw new BadMethodCallException(sprintf('Enum %s not found on %s', $name, get_class($this)));
+        return implode('|', self::getKeyArray($value));
     }
 
     /**
      * @param $value
-     *
-     * @throws \ReflectionException
      *
      * @return bool
      */
@@ -71,17 +37,36 @@ abstract class BitMask extends Enum
     }
 
     /**
-     * @throws \ReflectionException
+     * @param $name
+     * @param $arguments
      *
+     * @return bool|self
+     * @throws ReflectionException
+     *
+     */
+    public function __call($name, $arguments)
+    {
+        $array = static::toArray();
+        $regexBase = '/(isComponentOf|is|set)(%s)/m';
+        $regexFull = sprintf($regexBase, implode('$|', array_keys($array)));
+        preg_match($regexFull, $name, $match);
+        if (count($match) > 0 && $match[0] === $name) {
+            return $this->{$match[1] . 'Flag'}($array[$match[2]], $arguments[0] ?? true);
+        }
+
+        throw new BadMethodCallException(sprintf('Enum %s not found on %s', $name, get_class($this)));
+    }
+
+    /**
      * @return array
      */
     public static function toArray(): array
     {
         $firstTime = !isset(static::$cache[static::class]);
-        $array = array_filter(parent::toArray(), function ($value): bool {
+        $array = array_filter(parent::toArray(), static function ($value): bool {
             return is_scalar($value);
         });
-        $firstTime && array_walk($array, function ($item): void {
+        $firstTime && array_walk($array, static function ($item): void {
             if (!is_int($item)) {
                 throw new UnexpectedValueException(
                     sprintf('All defined Const on Enum %s should be integers', static::class)
@@ -95,9 +80,9 @@ abstract class BitMask extends Enum
     /**
      * @return string
      */
-    public function getKey()
+    public function getKey(): string
     {
-        return implode('|', $this->getKeyArray($this->value));
+        return implode('|', self::getKeyArray($this->value));
     }
 
     /**
@@ -105,28 +90,19 @@ abstract class BitMask extends Enum
      *
      * @return array
      */
-    public static function getKeyArray($value)
+    public static function getKeyArray(mixed $value): array
     {
-        $f = array_filter(static::toArray(), function ($key) use (&$value) {
+        $f = array_filter(static::toArray(), static function ($key) use (&$value) {
             return $value & $key;
         });
 
         return array_keys($f);
     }
 
-    protected static function assertValidValueReturningKey($value): string
-    {
-        if (!static::isValid($value)) {
-            throw new \UnexpectedValueException("Value '$value' is not part of the enum ".static::class);
-        }
-
-        return implode('|', self::getKeyArray($value));
-    }
-
     /**
-     * @throws \ReflectionException
-     *
      * @return string
+     * @throws ReflectionException
+     *
      */
     public function __toString(): string
     {
@@ -134,12 +110,12 @@ abstract class BitMask extends Enum
         $array = static::toArray();
         $ret = '';
         foreach ($array as $key => $value) {
-            $ret .= "'".$key."' => ".($this->{'is'.$key}() ? 'TRUE' : 'FALSE').PHP_EOL;
+            $ret .= "'" . $key . "' => " . ($this->{'is' . $key}() ? 'TRUE' : 'FALSE') . PHP_EOL;
         }
 
-        return $name.'['.PHP_EOL.
-            $ret.
-            ']'.PHP_EOL;
+        return $name . '[' . PHP_EOL .
+            $ret .
+            ']' . PHP_EOL;
     }
 
     public function getName(): string
@@ -147,5 +123,26 @@ abstract class BitMask extends Enum
         $path = explode('\\', __CLASS__);
 
         return array_pop($path);
+    }
+
+    protected function isFlag(int $flag): bool
+    {
+        return 0 === $flag ? 0 === $this->value : (($this->value & $flag) === $flag);
+    }
+
+    protected function isComponentOfFlag(int $flag): bool
+    {
+        return 0 === $flag ? $flag === $this->value : (($this->value & $flag) === $this->value);
+    }
+
+    protected function setFlag(int $flag, bool $value): static
+    {
+        if ($value) {
+            $this->value = 0 === $flag ? 0 : $this->value | $flag;
+        } else {
+            $this->value = 0 === $flag ? $this->value : $this->value & ~$flag;
+        }
+
+        return $this;
     }
 }
