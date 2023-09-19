7b5ee46c26e1019f8b21171ffe1887ec380e03d0
Christopher Harris <c.harris@multitel.com.au>
2023-09-19 17:13:43 +1000

    remove comparator

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 require_once dirname(__DIR__).'/vendor/autoload.php';
-if (class_exists(\MyCLabs\Enum\PHPUnit\Comparator::class)) {
+//if (class_exists(\MyCLabs\Enum\PHPUnit\Comparator::class)) {
 //    \SebastianBergmann\Comparator\Factory::getInstance()->register(new \MyCLabs\Enum\PHPUnit\Comparator());
-}
+//}
